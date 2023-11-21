7e2fa9f32d1863daca8cd19f9481599ec9b6715d
xiaozirun <zeng807046079@gmail.com>
2023-11-21 23:08:16 +0800

    add:错误结构体ErrDataIsEmpty

--- global/error.go
+++ global/error.go
@@ -14,6 +14,25 @@ var (
 	ErrInvalidParameter     = errors.New("invalid parameter")
 )
 
+// 数据校验
+var (
+	ErrDataIsEmpty = NewErrDataIsEmpty("")
+)
+
+type errDataIsEmpty struct {
+	Field string
+}
+
+func (e *errDataIsEmpty) Error() string {
+	return e.Field + "数据不可为空"
+}
+
+func NewErrDataIsEmpty(param string) error {
+	return &errDataIsEmpty{
+		Field: param,
+	}
+}
+
 var ErrOperationTooFrequent = errors.New("操作过于频繁,请稍后再试！")
 var ErrVerifyEmailCaptchaFail = errors.New("校验邮箱验证码失败！")
 var ErrServiceClosed = errors.New("服务未开启")
