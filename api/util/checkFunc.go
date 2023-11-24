802eda4f64a4525d34b4f177a536325075a85d76
xiaozirun <zeng807046079@gmail.com>
2023-11-24 18:10:23 +0800

    apiUtil.checkFunc增加TransactionCategoryBelong方法

--- api/util/checkFunc.go
+++ api/util/checkFunc.go
@@ -4,11 +4,13 @@ import (
 	"KeepAccount/api/response"
 	"KeepAccount/global"
 	accountModel "KeepAccount/model/account"
+	categoryModel "KeepAccount/model/category"
 	"github.com/gin-gonic/gin"
 )
 
 type _checkFunc interface {
 	AccountBelong(accountId interface{}, ctx *gin.Context) (bool, *accountModel.Account)
+	TransactionCategoryBelong(id interface{}, ctx *gin.Context) (pass bool, category *categoryModel.Category, account *accountModel.Account)
 }
 
 type checkFunc struct {
@@ -28,4 +30,22 @@ func (ckf *checkFunc) AccountBelong(accountId interface{}, ctx *gin.Context) (bo
 		return false, nil
 	}
 	return true, &account
+}
+
+func (ckf *checkFunc) TransactionCategoryBelong(id interface{}, ctx *gin.Context) (pass bool, category *categoryModel.Category, account *accountModel.Account) {
+	err := global.GvaDb.First(&category, id).Error
+	if err != nil {
+		response.FailToError(ctx, err)
+		return
+	}
+	account, err = category.GetAccount()
+	if err != nil {
+		response.FailToError(ctx, err)
+		return
+	}
+	if account.UserId != ContextFunc.GetUserId(ctx) {
+		response.Forbidden(ctx)
+		return
+	}
+	return true, category, account
 }
