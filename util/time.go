2ea6075258089376b4f1600bcaba9e6915cd0c13
xiaozirun <zeng807046079@gmail.com>
2024-01-10 18:16:08 +0800

    增加一些方法在util.time

--- util/time.go
+++ util/time.go
@@ -34,9 +34,52 @@ func (t *_time) SplitMonths(startDate, endDate time.Time) []time.Time {
 	return months
 }
 
+func (t *_time) SplitDays(startDate, endDate time.Time) []time.Time {
+	duration := endDate.Sub(startDate)
+	length := int(duration.Hours()/24) + 1
+	days := make([]time.Time, length, length)
+	startDate = time.Date(startDate.Year(), startDate.Month(), startDate.Day(), 0, 0, 0, 0, startDate.Location())
+
+	current := startDate
+	for i := 0; i < len(days); i++ {
+		days[i] = current
+		current = time.Date(current.Year(), current.Month(), current.Day()+1, 0, 0, 0, 0, current.Location())
+	}
+	return days
+}
+
+func (t *_time) GetFirstSecondOfMonth(date time.Time) time.Time {
+	year, month, _ := date.Date()
+	return time.Date(year, month, 1, 0, 0, 0, 0, date.Location())
+}
+
 func (t *_time) GetLastSecondOfMonth(date time.Time) time.Time {
 	year, month, _ := date.Date()
 	nextMonth := time.Date(year, month+1, 1, 0, 0, 0, 0, date.Location())
 	lastSecond := nextMonth.Add(-time.Second)
 	return lastSecond
+}
+
+// 获取本周一的第一秒
+func (t *_time) GetFirstSecondOfMonday(currentTime time.Time) time.Time {
+	weekday := currentTime.Weekday()
+	// 计算需要减去的天数，使得得到本周一的日期
+	daysToMonday := time.Duration(0)
+	if weekday != time.Monday {
+		daysToMonday = time.Duration(weekday - time.Monday)
+		if weekday < time.Monday {
+			daysToMonday += 7 // 如果当前是周日，则需要向前推算7天
+		}
+	}
+
+	monday := currentTime.Add(-daysToMonday * 24 * time.Hour)
+	monday = time.Date(monday.Year(), monday.Month(), monday.Day(), 0, 0, 0, 0, currentTime.Location())
+	return monday
+}
+
+// 获取今年的第一秒
+func (t *_time) GetFirstSecondOfYear(currentTime time.Time) time.Time {
+	year := currentTime.Year()
+	firstSecondOfYear := time.Date(year, time.January, 1, 0, 0, 0, 0, currentTime.Location())
+	return firstSecondOfYear
 }
