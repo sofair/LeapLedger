a9a8bffa269c5c2fab3d5ca909b0c0838ce11171
xiaozirun <zeng807046079@gmail.com>
2024-10-15 14:43:44 +0800

    changes workflows

--- docs/updateDocs.sh
+++ docs/updateDocs.sh
@@ -1,7 +1,7 @@
 #!/sh
 echo go install github.com/swaggo/swag/cmd/swag@latest
 go install github.com/swaggo/swag/cmd/swag@latest
-echo go run /go/LeapLedger/docs/beforeDocsMake/renameModel/main.go
-go run /go/LeapLedger/docs/beforeDocsMake/renameModel/main.go
+#echo go run /go/LeapLedger/docs/beforeDocsMake/renameModel/main.go
+#go run /go/LeapLedger/docs/beforeDocsMake/renameModel/main.go
 echo swag init -p pascalcase
 swag init -p pascalcase
